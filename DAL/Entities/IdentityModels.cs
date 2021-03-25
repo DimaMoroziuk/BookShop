e27083fd332f06bca22372f7a270631dc2cb02cb
DimaMoroziuk <moroziuk.dmytro@gmail.com>
2021-03-25 20:23:06 +0200

    Registration and Login were added

---
+++ DAL/Entities/IdentityModels.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class ApplicationUser: IdentityUser
+    {
+        public string FullName { get; set; }
+    }
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser> {
+        public ApplicationDbContext():base("DbShopBooks", throwIfV1Schema: false) { 
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ApplicationUser>().ToTable("User");
+            modelBuilder.Entity<IdentityRole>().ToTable("Role");
+            modelBuilder.Entity<IdentityUserRole>().ToTable("UserRole");
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("UserClaim");
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("UserLogin");
+        }
+    }
+}
